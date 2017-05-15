fb4c6a45975d53d12c30000c850263f2e66e8711
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-05-14 22:51:18 -0400

    scrabble-score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,24 @@
+package scrabble
+
+import "strings"
+
+const testVersion = 5
+
+var points = map[rune]int{
+	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+	'd': 2, 'g': 2,
+	'b': 3, 'c': 3, 'm': 3, 'p': 3,
+	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+	'k': 5,
+	'j': 8, 'x': 8,
+	'q': 10, 'z': 10,
+}
+
+// Score calculates the Scrabble for for a given word
+func Score(word string) (score int) {
+	for _, letter := range strings.ToLower(word) {
+		score += points[letter]
+	}
+
+	return
+}
