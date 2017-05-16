00dc3a96d7264669c0b304e2953ab4c8b0828a5d
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-05-16 12:01:27 -0400

    cleaner implementation of luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,27 +1,28 @@
 package luhn
 
 import (
-	"fmt"
 	"strings"
+	"unicode"
 )
 
 const testVersion = 2
 
 // Valid determines whether or not a number is valid per the Luhn formula.
 func Valid(number string) bool {
-	number = strings.Replace(number, " ", "", -1)
+	number = reverse(strings.Replace(number, " ", "", -1))
 
-	sum := 0
-	index := len(number) - 1
-
-	if index < 1 {
+	if len(number) <= 1 {
 		return false
 	}
 
-	for i := range number {
-		digit := int(number[index] - '0')
+	sum := 0
 
-		fmt.Println(digit)
+	for i, r := range number {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+
+		digit := int(r - '0')
 		if i%2 == 1 {
 			digit = digit * 2
 			if digit > 9 {
@@ -30,8 +31,17 @@ func Valid(number string) bool {
 		}
 
 		sum += digit
-		index--
 	}
 
 	return sum%10 == 0
+}
+
+func reverse(input string) string {
+	r := []rune(input)
+
+	for i := 0; i < len(r)/2; i++ {
+		r[i], r[len(r)-i-1] = r[len(r)-i-1], r[i]
+	}
+
+	return string(r)
 }
