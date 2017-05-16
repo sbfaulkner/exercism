4b53f6d8dc97bc05aea8f6907b026dbf9b697130
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-05-16 10:20:50 -0400

    crypto-square

---
+++ go/crypto-square/crypto_square.go
@@ -0,0 +1,31 @@
+package cryptosquare
+
+import (
+	"math"
+	"strings"
+	"unicode"
+)
+
+const testVersion = 2
+
+// Encode uses the classic square code method to encrypt a message.
+func Encode(text string) string {
+	normalize := func(r rune) rune {
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+			return -1
+		}
+		return unicode.ToLower(r)
+	}
+
+	text = strings.Map(normalize, text)
+	columns := int(math.Ceil(math.Sqrt(float64(len(text)))))
+	square := make([]string, columns)
+
+	for c := 0; c < columns; c++ {
+		for r := c; r < len(text); r += columns {
+			square[c] += text[r : r+1]
+		}
+	}
+
+	return strings.Join(square, " ")
+}
