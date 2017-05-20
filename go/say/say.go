bfc3a550e193b64b54b65c6cc2b0f6c7d7082a02
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-05-20 18:16:00 -0400

    say - step 1

---
+++ go/say/say.go
@@ -0,0 +1,38 @@
+package say
+
+import "fmt"
+
+const testVersion = 1
+
+var numbers = [][]string{
+	{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"},
+	{"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"},
+	{"twenty"},
+	{"thirty"},
+	{"forty"},
+	{"fifty"},
+	{"sixty"},
+	{"seventy"},
+	{"eighty"},
+	{"ninety"},
+}
+
+// var powers = []string{"one", "ten", "hundred", "thousand", "million", "billion"}
+
+// Say converts a number to text.
+func Say(number uint64) string {
+	var text string
+
+	tens := number / 10
+	ones := number % 10
+
+	if ones < uint64(len(numbers[tens])) {
+		text = numbers[tens][ones]
+	} else if tens > 0 {
+		text = fmt.Sprintf("%s-%s", numbers[tens][0], numbers[0][ones])
+	} else {
+		text = numbers[0][ones]
+	}
+
+	return text
+}
