e8af84e5ca8c5c6423741a979bd646af55cf19f7
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-05-20 20:57:00 -0400

    support hundreds

--- go/say/say.go
+++ go/say/say.go
@@ -1,8 +1,10 @@
 package say
 
-import "fmt"
-
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
 const testVersion = 1
 
@@ -23,20 +25,33 @@ var numbers = [][]string{
 
 // Say converts a number to text.
 func Say(number uint64) string {
-	var text string
-
-	tens := number / 10
-	ones := number % 10
-
-	if ones < uint64(len(numbers[tens])) {
-		text = numbers[tens][ones]
-	} else if tens > 0 {
-		text = fmt.Sprintf("%s-%s", numbers[tens][0], numbers[0][ones])
-	} else {
-		text = numbers[0][ones]
-	}
+	text := spell(number)
 
 	exec.Command("say", text).Run()
 
 	return text
+}
+
+func spell(number uint64) string {
+	var values []string
+
+	hundreds := number / 100
+	tens := number % 100 / 10
+	ones := number % 10
+
+	if hundreds > 0 {
+		values = append(values, numbers[0][hundreds], "hundred")
+	}
+
+	if ones < uint64(len(numbers[tens])) {
+		if ones > 0 || tens > 0 || number == 0 {
+			values = append(values, numbers[tens][ones])
+		}
+	} else if tens > 0 {
+		values = append(values, fmt.Sprintf("%s-%s", numbers[tens][0], numbers[0][ones]))
+	} else {
+		values = append(values, numbers[0][ones])
+	}
+
+	return strings.Join(values, " ")
 }
