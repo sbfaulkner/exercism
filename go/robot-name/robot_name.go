c00cb9b651dce10eae3bcd1c8ee212c365cdcaa1
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-05-21 16:18:52 -0400

    prevent duplicate names

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -13,14 +13,31 @@ type Robot struct {
 	name string
 }
 
+// Name returns the robot's random name (assigning it if necessary).
 func (robot *Robot) Name() string {
 	if robot.name == "" {
-		rand.Seed(time.Now().UnixNano())
-		robot.name = fmt.Sprintf("%c%c%03d", 'A'+rune(rand.Intn(26)), 'A'+rune(rand.Intn(26)), rand.Intn(1000))
+		robot.name = randomName()
 	}
 	return robot.name
 }
 
+// Reset clears the robot's settings.
 func (robot *Robot) Reset() {
 	robot.name = ""
+}
+
+var names = map[string]bool{}
+
+func randomName() (name string) {
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		name = fmt.Sprintf("%c%c%03d", 'A'+rune(rand.Intn(26)), 'A'+rune(rand.Intn(26)), rand.Intn(1000))
+		if !names[name] {
+			names[name] = true
+			break
+		}
+	}
+
+	return name
 }
