33e957fcff685c3d1bfc958268ffd05f5ad49d4e
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-06-11 15:27:49 -0400

    tree-building: refactor

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -3,6 +3,7 @@ package tree
 import (
 	"errors"
 	"fmt"
+	"sort"
 )
 
 const testVersion = 4
@@ -19,91 +20,79 @@ type Node struct {
 type Mismatch struct{}
 
 func (m Mismatch) Error() string {
-	return "c"
+	return "incorrect number of nodes"
 }
 
 func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
+
 	root := &Node{}
 	todo := []*Node{root}
 	n := 1
+
 	for {
 		if len(todo) == 0 {
 			break
 		}
+
 		newTodo := []*Node(nil)
-		for _, c := range todo {
+
+		for _, t := range todo {
 			for _, r := range records {
-				if r.Parent == c.ID {
-					if r.ID < c.ID {
-						return nil, errors.New("a")
-					} else if r.ID == c.ID {
+				if r.Parent == t.ID {
+					if r.ID < t.ID {
+						return nil, errors.New("parent exceeds id")
+					}
+
+					if r.ID == t.ID {
 						if r.ID != 0 {
-							return nil, fmt.Errorf("b")
+							return nil, fmt.Errorf("node references self")
 						}
 					} else {
 						n++
-						switch len(c.Children) {
-						case 0:
-							nn := &Node{ID: r.ID}
-							c.Children = []*Node{nn}
-							newTodo = append(newTodo, nn)
-						case 1:
-							nn := &Node{ID: r.ID}
-							if c.Children[0].ID < r.ID {
-								c.Children = []*Node{c.Children[0], nn}
-								newTodo = append(newTodo, nn)
-							} else {
-								c.Children = []*Node{nn, c.Children[0]}
-								newTodo = append(newTodo, nn)
-							}
-						default:
-							nn := &Node{ID: r.ID}
-							newTodo = append(newTodo, nn)
-						breakpoint:
-							for range []bool{false} {
-								for i, cc := range c.Children {
-									if cc.ID > r.ID {
-										a := make([]*Node, len(c.Children)+1)
-										copy(a, c.Children[:i])
-										copy(a[i+1:], c.Children[i:])
-										copy(a[i:i+1], []*Node{nn})
-										c.Children = a
-										break breakpoint
-									}
-								}
-								c.Children = append(c.Children, nn)
-							}
-						}
+						nn := &Node{ID: r.ID}
+						newTodo = append(newTodo, nn)
+
+						t.addChild(nn)
 					}
 				}
 			}
 		}
 		todo = newTodo
 	}
+
 	if n != len(records) {
 		return nil, Mismatch{}
 	}
-	if err := chk(root, len(records)); err != nil {
+
+	if err := root.chk(len(records)); err != nil {
 		return nil, err
 	}
+
 	return root, nil
 }
 
-func chk(n *Node, m int) (err error) {
+func (n *Node) addChild(child *Node) {
+	pos := sort.Search(len(n.Children), func(i int) bool { return n.Children[i].ID >= child.ID })
+	n.Children = append(n.Children[0:pos], append([]*Node{child}, n.Children[pos:]...)...)
+}
+
+func (n *Node) chk(m int) error {
 	if n.ID > m {
 		return fmt.Errorf("z")
-	} else if n.ID == m {
-		return fmt.Errorf("y")
-	} else {
-		for i := 0; i < len(n.Children); i++ {
-			err = chk(n.Children[i], m)
-			if err != nil {
-				return
-			}
-		}
-		return
 	}
+
+	if n.ID == m {
+		return fmt.Errorf("y")
+	}
+
+	for _, c := range n.Children {
+		if err := c.chk(m); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
