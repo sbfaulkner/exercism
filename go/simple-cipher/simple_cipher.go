f13ed4c5f63378e325efad11c231f59eb82f743a
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-06-12 09:54:26 -0400

    simple-cipher: implement vignere cipher and refactor shift and caesar in terms of vigenere

--- go/simple-cipher/simple_cipher.go
+++ go/simple-cipher/simple_cipher.go
@@ -1,42 +1,19 @@
-// For Step 1, implement the Caesar cipher, which seems clear enough except
-// maybe for figuring out whether to add or subtract.
-//
-// For Step 2, implement a shift cipher, like Caesar except the shift amount
-// is specified as an int.  (Each letter of "ddddddddddddddddd", is 'd';
-// 'd'-'a' == 3, so the corresponding shift amount is 3.)
-//
-// Steps 2 and 3 seem to be describing the Vigenère cipher (Google it)
-// so let's do that too.  The random thing, don't worry about.  There is
-// no test for that.
-//
-// API:
-//
-// NewCaesar() Cipher
-// NewShift(int) Cipher
-// NewVigenere(string) Cipher
-//
-// Interface Cipher is in file cipher.go.
-//
-// Argument for NewShift must be in the range 1 to 25 or -1 to -25.
-// Zero is disallowed.  For invalid arguments NewShift returns nil.
-//
-// Argument for NewVigenere must consist of lower case letters a-z only.
-// Values consisting entirely of the letter 'a' are disallowed.
-// For invalid arguments NewVigenere returns nil.
-
 package cipher
 
 import (
 	"regexp"
+	"strings"
 	"unicode"
 )
 
 const testVersion = 1
 
-type vigenereCipher []rune
+type vigenereCipher struct {
+	key []rune
+}
 
 type shiftCipher struct {
-	shift int
+	vigenereCipher
 }
 
 type caesarCipher struct {
@@ -45,7 +22,7 @@ type caesarCipher struct {
 
 // NewCaesar returns a cipher that implements the Caesar cipher
 func NewCaesar() Cipher {
-	return caesarCipher{shiftCipher{3}}
+	return caesarCipher{shiftCipher{vigenereCipher{[]rune{3}}}}
 }
 
 // NewShift returns a cipher thtn implements a shift cipher
@@ -54,34 +31,53 @@ func NewShift(s int) Cipher {
 		return nil
 	}
 
-	return shiftCipher{s}
+	return shiftCipher{vigenereCipher{[]rune{rune(s)}}}
 }
 
 // NewVigenere returns a cipher that implements the Vigenère cipher
 func NewVigenere(k string) Cipher {
+	if len(k) == 0 {
+		return nil
+	}
+
+	if k == strings.Repeat("a", len(k)) {
+		return nil
+	}
+
+	for _, r := range k {
+		if !unicode.IsLower(r) {
+			return nil
+		}
+	}
+
 	key := make([]rune, len(k))
 
 	for i, b := range k {
 		key[i] = b - 'a'
 	}
 
-	return vigenereCipher(key)
-}
-
-func (c shiftCipher) Encode(input string) string {
-	return shift(input, c.shift)
-}
-
-func (c shiftCipher) Decode(input string) string {
-	return shift(input, -c.shift)
+	return vigenereCipher{key}
 }
 
 func (c vigenereCipher) Encode(input string) string {
-	return input
+	return c.shift(input, 1)
 }
 
 func (c vigenereCipher) Decode(input string) string {
-	return input
+	return c.shift(input, -1)
+}
+
+func (c vigenereCipher) shift(input string, d int) string {
+	input = normalize(input)
+
+	output := make([]rune, len(input))
+
+	for i, r := range input {
+		s := c.key[i%len(c.key)] * rune(d)
+		output[i] = (r-'a'+s+26)%26 + 'a'
+	}
+
+	return string(output)
 }
 
 var regexpAlpha = regexp.MustCompile(`[^a-z]`)
@@ -93,16 +89,4 @@ func normalize(input string) (output string) {
 		}
 	}
 	return
-}
-
-func shift(input string, s int) string {
-	input = normalize(input)
-
-	output := make([]rune, len(input))
-
-	for i, r := range input {
-		output[i] = (r-'a'+rune(s)+26)%26 + 'a'
-	}
-
-	return string(output)
 }
