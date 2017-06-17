a7a8e6a2586f0d1f6b9fd22b197782a5c0b5022a
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-06-16 22:54:00 -0400

    pov: comments

--- go/pov/pov.go
+++ go/pov/pov.go
@@ -7,21 +7,25 @@ import (
 
 const testVersion = 2
 
+// Graph is a directed graph
+type Graph struct {
+	root  *node
+	nodes map[string]*node
+}
+
+// node is a node within a directed graph
 type node struct {
 	label    string
 	parent   *node
 	children []*node
 }
 
-type Graph struct {
-	root  *node
-	nodes map[string]*node
-}
-
+// New creates a new (empty) directed graph
 func New() *Graph {
 	return &Graph{nodes: map[string]*node{}}
 }
 
+// AddNode adds a node with the specified label to the graph
 func (g *Graph) AddNode(nodeLabel string) {
 	if g.nodes[nodeLabel] != nil {
 		panic(errors.New("duplicate node label"))
@@ -30,18 +34,7 @@ func (g *Graph) AddNode(nodeLabel string) {
 	g.addNode(nodeLabel)
 }
 
-func (g *Graph) addNode(nodeLabel string) *node {
-	n := &node{label: nodeLabel}
-
-	g.nodes[nodeLabel] = n
-
-	if g.root == nil {
-		g.root = n
-	}
-
-	return n
-}
-
+// AddArc creates an arc between the two specified nodes (adding the nodes if necessary)
 func (g *Graph) AddArc(from, to string) {
 	f := g.nodes[from]
 	if f == nil {
@@ -61,6 +54,7 @@ func (g *Graph) AddArc(from, to string) {
 	}
 }
 
+// ArcList returns a string representation of all arcs in the directed graph
 func (g *Graph) ArcList() []string {
 	if g.root == nil {
 		return []string{}
@@ -69,17 +63,7 @@ func (g *Graph) ArcList() []string {
 	return g.root.arcList()
 }
 
-func (n *node) arcList() []string {
-	a := []string{}
-
-	for _, c := range n.children {
-		a = append(a, fmt.Sprintf("%s -> %s", n.label, c.label))
-		a = append(a, c.arcList()...)
-	}
-
-	return a
-}
-
+// ChangeRoot returns a new directed graph with the specified (new) root
 func (g *Graph) ChangeRoot(oldRoot, newRoot string) *Graph {
 	ng := New()
 
@@ -91,6 +75,20 @@ func (g *Graph) ChangeRoot(oldRoot, newRoot string) *Graph {
 	return ng
 }
 
+// addNode adds a node with the specifie label to the graph
+func (g *Graph) addNode(nodeLabel string) *node {
+	n := &node{label: nodeLabel}
+
+	g.nodes[nodeLabel] = n
+
+	if g.root == nil {
+		g.root = n
+	}
+
+	return n
+}
+
+// copy recursively copies a node and its descendants to the graph
 func (g *Graph) copy(n *node) {
 	for _, c := range n.children {
 		if g.nodes[c.label] != nil {
@@ -102,6 +100,7 @@ func (g *Graph) copy(n *node) {
 	}
 }
 
+// invert inverts the relationship between a node and its parent
 func (g *Graph) invert(n *node) {
 	if n.parent == nil {
 		return
@@ -110,4 +109,16 @@ func (g *Graph) invert(n *node) {
 	g.AddArc(n.label, n.parent.label)
 	g.copy(n.parent)
 	g.invert(n.parent)
+}
+
+// arcList recursively generates string representations of the arcs starting at the current node
+func (n *node) arcList() []string {
+	a := []string{}
+
+	for _, c := range n.children {
+		a = append(a, fmt.Sprintf("%s -> %s", n.label, c.label))
+		a = append(a, c.arcList()...)
+	}
+
+	return a
 }
