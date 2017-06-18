aa54508d5bc833d8a825b265291eb30fc5acfde2
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-06-17 23:12:01 -0400

    minesweeper: take 2

--- go/minesweeper/minesweeper.go
+++ go/minesweeper/minesweeper.go
@@ -3,40 +3,68 @@ package minesweeper
 import (
 	"errors"
 	"regexp"
-	"strings"
 )
 
 const testVersion = 1
 
 // Board errors
 var (
-	ErrInvalidWidth = errors.New("minesweeper: invalid board width")
-	ErrInvalidEdge  = errors.New("minesweeper: invalid horizontal edge")
-	ErrInvalidRow   = errors.New("minesweeper: invalid vertical edge")
+	ErrInvalidWidth          = errors.New("minesweeper: invalid board width")
+	ErrInvalidCorner         = errors.New("minesweeper: invalid corner")
+	ErrInvalidHorizontalEdge = errors.New("minesweeper: invalid horizontal edge")
+	ErrInvalidVerticalEdge   = errors.New("minesweeper: invalid vertical edge")
+	ErrInvalidCell           = errors.New("minesweeper: invalid cell")
 )
 
 var (
 	validEdge = regexp.MustCompile(`^\+-*\+$`)
-	validRow  = regexp.MustCompile(`^\|[ *]*\|$`)
+	validRow  = regexp.MustCompile(`^\|[ *1-8]*\|$`)
 )
 
 // Count marks the mine count for all spaces on the board
 func (b *Board) Count() error {
-	l := len(*b)
-	w := len((*b)[0])
+	l, w := len(*b), len((*b)[0])
 
-	for r := 0; r < l; r++ {
-		if err := b.validateRow(r, w, l); err != nil {
-			return err
+	for r, row := range *b {
+		if len(row) != w {
+			return ErrInvalidWidth
 		}
 
-		for c := 1; c < len((*b)[r])-1; c++ {
-			if (*b)[r][c] != ' ' {
-				continue
-			}
-
-			if m := b.count(r, c); m > 0 {
-				(*b)[r][c] = '0' + byte(m)
+		for c, cell := range row {
+			switch cell {
+			case '+':
+				if r != 0 && r != l-1 || c != 0 && c != w-1 {
+					return ErrInvalidCell
+				}
+			case '-':
+				if r != 0 && r != l-1 || c == 0 || c == w-1 {
+					return ErrInvalidCell
+				}
+			case '|':
+				if c != 0 && c != w-1 || r == 0 || r == l-1 {
+					return ErrInvalidCell
+				}
+			case '*':
+				if r == 0 || r == l-1 {
+					if c == 0 || c == w-1 {
+						return ErrInvalidCorner
+					}
+					return ErrInvalidHorizontalEdge
+				}
+				if c == 0 || c == w-1 {
+					return ErrInvalidVerticalEdge
+				}
+				b.increment(r-1, c-1)
+				b.increment(r-1, c)
+				b.increment(r-1, c+1)
+				b.increment(r, c-1)
+				b.increment(r, c+1)
+				b.increment(r+1, c-1)
+				b.increment(r+1, c)
+				b.increment(r+1, c+1)
+			case ' ', '1', '2', '3', '4', '5', '6', '7', '8':
+			default:
+				return ErrInvalidCell
 			}
 		}
 	}
@@ -44,32 +72,13 @@ func (b *Board) Count() error {
 	return nil
 }
 
-func (b Board) count(r, c int) (n int) {
-	n += strings.Count(string(b[r-1][c-1:c+2]), "*")
-	n += strings.Count(string(b[r][c-1:c+2]), "*")
-	n += strings.Count(string(b[r+1][c-1:c+2]), "*")
+func (b Board) increment(r, c int) {
+	switch b[r][c] {
+	case ' ':
+		b[r][c] = '1'
+	case '1', '2', '3', '4', '5', '6', '7':
+		b[r][c]++
+	}
 
 	return
-}
-
-func (b Board) validateRow(r, w, l int) error {
-	row := b[r]
-
-	if len(row) != w {
-		return ErrInvalidWidth
-	}
-
-	l--
-
-	if r%l == 0 {
-		if !validEdge.Match(row) {
-			return ErrInvalidEdge
-		}
-	} else {
-		if !validRow.Match(row) {
-			return ErrInvalidRow
-		}
-	}
-
-	return nil
 }
