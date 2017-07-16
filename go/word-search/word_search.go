6b627be906d482f297376edbc2ad5717277b68e2
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-07-15 20:11:33 -0400

    word-search: directions represented as explicit vectors

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -27,26 +27,27 @@ func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 }
 
 // Direction represents the directions to match words
-type Direction int
+type Direction struct {
+	dx, dy int
+}
 
-// Direction constants for matching
-const (
-	E Direction = iota
-	NE
-	N
-	NW
-	W
-	SW
-	S
-	SE
-)
+var directions = []Direction{
+	{1, 0},
+	{1, -1},
+	{0, -1},
+	{-1, -1},
+	{-1, 0},
+	{-1, 1},
+	{0, 1},
+	{1, 1},
+}
 
 // Search finds a specific word in the Puzzle
 func (puzzle Puzzle) Search(word string) ([2][2]int, error) {
 	for r, row := range puzzle {
 		for c, letter := range row {
 			if letter == rune(word[0]) {
-				for d := E; d < SE; d++ {
+				for _, d := range directions {
 					if last, ok := puzzle.match(word, r, c, d); ok {
 						return [2][2]int{[2]int{c, r}, last}, nil
 					}
@@ -60,51 +61,21 @@ func (puzzle Puzzle) Search(word string) ([2][2]int, error) {
 
 // match checks a Puzzle location for a word in a specific Direction
 func (puzzle Puzzle) match(word string, r, c int, d Direction) (last [2]int, ok bool) {
-	dx, dy := deltas(d)
-
-	c2 := c + dx*(len(word)-1)
+	c2 := c + d.dx*(len(word)-1)
 	if c2 < 0 || c2 >= len(puzzle[0]) {
 		return
 	}
 
-	r2 := r + dy*(len(word)-1)
+	r2 := r + d.dy*(len(word)-1)
 	if r2 < 0 || r2 >= len(puzzle) {
 		return
 	}
 
 	for i := 1; i < len(word); i++ {
-		if puzzle[r+i*dy][c+i*dx] != word[i] {
+		if puzzle[r+i*d.dy][c+i*d.dx] != word[i] {
 			return
 		}
 	}
 
 	return [2]int{c2, r2}, true
-}
-
-// deltas is a helper to return the direction vector for matching in a specific Direction
-func deltas(d Direction) (dx int, dy int) {
-	switch d {
-	case E:
-		dx = 1
-	case NE:
-		dx = 1
-		dy = -1
-	case N:
-		dy = -1
-	case NW:
-		dx = -1
-		dy = -1
-	case W:
-		dx = -1
-	case SW:
-		dx = -1
-		dy = 1
-	case S:
-		dy = 1
-	case SE:
-		dx = 1
-		dy = 1
-	}
-
-	return
 }
