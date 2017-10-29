75b18d069390063617c6f7d3439a952319c27491
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-10-29 19:43:14 -0400

    lua/raindrops: refactor for readability

--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,14 +1,14 @@
 local factors = {
-  {3, "Pling"},
-  {5, "Plang"},
-  {7, "Plong"},
+  { factor = 3, sound = "Pling" },
+  { factor = 5, sound = "Plang" },
+  { factor = 7, sound = "Plong" },
 }
 
 function raindrops(number)
   text = ''
 
-  for i = 1, #factors do
-    if (number % factors[i][1]) == 0 then text = text .. factors[i][2] end
+  for _, f in ipairs(factors) do
+    if (number % f.factor) == 0 then text = text .. f.sound end
   end
 
   return #text > 0 and text or tostring(number)
