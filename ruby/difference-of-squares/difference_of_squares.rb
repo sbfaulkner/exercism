6ae807d793f8a24e59f9f02dc5f364dc6e3660ed
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-10-29 14:51:55 -0400

    ruby/difference-of-squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,26 @@
+# Squares defines methods to calculate the Difference Of Squares
+class Squares
+  def initialize(number)
+    @number = number
+  end
+
+  # square_of_sum calculates the square of the sum the terms in the series
+  def square_of_sum
+    (1..@number).sum**2
+  end
+
+  # square_of_sum calculates the sum of the squares of the terms in the series
+  def sum_of_squares
+    (1..@number).map { |n| n**2 }.sum
+  end
+
+  # difference calculates the difference between
+  # the square of the sum of the terms and the sum of the squares of the terms
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
