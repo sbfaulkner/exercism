cc789d6fa7a08859e78f7f8c336940615fe219a6
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-10-29 22:05:32 -0400

    lua/bob: refactor with simpler test for question mark

--- lua/bob/bob.lua
+++ lua/bob/bob.lua
@@ -3,7 +3,7 @@ local bob = {}
 function bob.hey(text)
   if #text == 0 then return 'Fine, be that way.' end
 
-  if text:sub(#text, #text) == '?' then return 'Sure' end
+  if text:sub(-1) == '?' then return 'Sure' end
 
   if text:upper() == text then return 'Whoa, chill out!' end
 
