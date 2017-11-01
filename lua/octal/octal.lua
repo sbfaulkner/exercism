59398b9c91009d63d14b4d8d6f27612a699049e2
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-10-31 20:20:28 -0400

    lua/octal: refactor

--- lua/octal/octal.lua
+++ lua/octal/octal.lua
@@ -1,15 +1,13 @@
-function Octal(value)
-  local to_decimal = function ()
-                       decimal = 0
-                       for i = 1, #value do
-                         digit = tonumber(value:sub(i,i))
-                         if digit == nil or digit > 7 then return 0 end
-                         decimal = decimal * 8 + digit
-                       end
-                       return decimal
-                     end
-
-  return { to_decimal = to_decimal }
+return function (value)
+  return {
+    to_decimal = function ()
+      local decimal = 0
+      for i = 1, #value do
+        local digit = tonumber(value:sub(i,i))
+        if digit == nil or digit > 7 then return 0 end
+        decimal = decimal * 8 + digit
+      end
+      return decimal
+    end
+  }
 end
-
-return Octal
