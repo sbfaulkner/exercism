a1e2b625fae924b893f229d024c4225ec3aaf141
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-03 20:52:26 -0400

    bash/pangram: use sequence expansion for alphabet

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -1,7 +1,5 @@
 SENTENCE=${1^^*}
 
-ALPHABET=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
-
-for LETTER in ${ALPHABET[@]}; do
+for LETTER in {A..Z}; do
   [[ $SENTENCE =~ $LETTER ]] || exit 1
 done
