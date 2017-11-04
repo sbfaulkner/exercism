f7049a4bc4f3ff42abb7377a3a68d5145493c615
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-03 22:09:37 -0400

    bash/hamming: no need to split strings... just index the characters

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,17 +1,13 @@
-split() {
-  echo $1 | fold -w 1
-}
-
 if [[ $# -ne 2 ]]; then
   echo "Usage: hamming.sh <string1> <string2>"
   exit 1
 fi
 
-STRAND1=($(split $1))
-STRAND2=($(split $2))
-LENGTH=${#STRAND1[@]}
+STRAND1=$1
+STRAND2=$2
+LENGTH=${#STRAND1}
 
-if [[ ${LENGTH} -ne ${#STRAND2[@]} ]]; then
+if [[ ${LENGTH} -ne ${#STRAND2} ]]; then
   echo "The two strands must have the same length."
   exit 1
 fi
@@ -19,7 +15,7 @@ fi
 DISTANCE=0
 
 for (( i=0; i < ${LENGTH}; i++ )); do
-  [[ ${STRAND1[$i]} != ${STRAND2[$i]} ]] && let DISTANCE+=1
+  [[ ${STRAND1:$i:1} != ${STRAND2:$i:1} ]] && let DISTANCE++
 done
 
 echo $DISTANCE
