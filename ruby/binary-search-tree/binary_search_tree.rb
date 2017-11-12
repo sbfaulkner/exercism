ce292df08f7dbd1dd105f9e313434f53eabd953b
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-12 15:03:38 -0500

    ruby/binary-search-tree: refactor

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -1,34 +1,27 @@
-module FrozenReader
-  private
-
-  def frozen_reader(*attrs)
-    attrs.each do |attr|
-      class_eval <<-SUBTREE_READER
-        def #{attr}
-          @#{attr}.dup.freeze
-        end
-      SUBTREE_READER
-    end
-  end
-end
-
 class Bst
-  extend FrozenReader
-
   attr_reader :data
-  frozen_reader :left, :right
 
   def initialize(data)
     @data = data
   end
 
-  def insert(data)
-    insert_or_new = lambda { |bst| bst ? bst.insert(data) : self.class.new(data) }
+  %w(left right).each do |subtree|
+    class_eval <<-SUBTREE_METHODS
+      def #{subtree}
+        @#{subtree}.dup.freeze
+      end
 
+      def insert_#{subtree}(data)
+        @#{subtree} = @#{subtree} ? @#{subtree}.insert(data) : self.class.new(data)
+      end
+    SUBTREE_METHODS
+  end
+
+  def insert(data)
     if data <= @data
-      @left = insert_or_new.call(@left)
+      insert_left(data)
     else
-      @right = insert_or_new.call(@right)
+      insert_right(data)
     end
 
     self
