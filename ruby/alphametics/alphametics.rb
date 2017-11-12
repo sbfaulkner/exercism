feef00033a5b2a483671eaaef5eff9244d2efbd2
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-12 16:53:45 -0500

    ruby/alphametics

---
+++ ruby/alphametics/alphametics.rb
@@ -0,0 +1,39 @@
+module Alphametics
+  class << self
+    DIGITS = ('0'..'9').to_a
+
+    EQUALS = ' == '
+    PLUS = ' + '
+
+    def solve(puzzle)
+      letters = puzzle.tr('^A-Z', '').chars.uniq.sort.join
+
+      left, right = puzzle.split(EQUALS)
+      left = left.split(PLUS)
+
+      DIGITS.permutation(letters.size).map(&:join).each do |digits|
+        actual = left.sum { |l| convert_letters_to_integer(l, letters, digits) || break }
+        next unless actual
+
+        expected = convert_letters_to_integer(right, letters, digits)
+        next unless expected
+
+        return Hash[letters.chars.zip(digits.chars.map(&:to_i))] if actual == expected
+      end
+
+      {}
+    end
+
+    private
+
+    def convert_letters_to_integer(term, variables, digits)
+      text = term.tr(variables, digits)
+      return nil if text.size > 1 && text[0] == '0'
+      text.to_i
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
