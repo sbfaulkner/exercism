c9443d66d27e4063f84080cd3146ae74c0cc5e45
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-11 22:06:37 -0500

    ruby/clock

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,28 @@
+class Clock
+  def self.at(hour, minute)
+    Clock.new(hour, minute)
+  end
+
+  attr_reader :hour, :minute
+
+  def initialize(hour, minute)
+    @hour = (hour + minute / 60) % 24
+    @minute = minute % 60
+  end
+
+  def to_s
+    format('%02d:%02d', hour, minute)
+  end
+
+  def +(minutes)
+    Clock.new(@hour, @minute + minutes)
+  end
+
+  def ==(clock)
+    clock.hour == hour && clock.minute == minute
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
