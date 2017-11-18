e391b003e0a4ee4d128c893148ebbd6db840ecc7
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-18 12:11:13 -0500

    ruby/saddle-points: refactor

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -3,19 +3,29 @@ class Matrix
 
   def initialize(text)
     @matrix = text.each_line.map { |row| row.split.map(&:to_i) }
-    @rows = @matrix.dup
-    @columns = @matrix.transpose
+    @rows = @matrix.dup.freeze
+    @columns = @matrix.transpose.freeze
   end
 
   def saddle_points
-    saddle_points = []
+    cells.select { |r, c| saddle_point?(r, c) }
+  end
 
-    rows.each_with_index do |row, r|
-      row.each_with_index do |value, c|
-        saddle_points << [r, c] if value == row.max && value == columns[c].min
-      end
-    end
+  private
 
-    saddle_points
+  def cells
+    (0...rows.count).to_a.product((0...columns.count).to_a)
+  end
+
+  def max_in_row?(r, c)
+    rows[r][c] == rows[r].max
+  end
+
+  def min_in_column?(r, c)
+    columns[c][r] == columns[c].min
+  end
+
+  def saddle_point?(r, c)
+    max_in_row?(r, c) && min_in_column?(r, c)
   end
 end
