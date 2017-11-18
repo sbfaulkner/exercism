a9d8a5b5b5e63523e2ec8a92d8c8e944bac5d709
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-11-18 11:41:20 -0500

    ruby/matrix: use a couple of extra builtins

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,7 +2,7 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(text)
-    @rows = text.split("\n").map { |row| row.split.map(&:to_i) }.freeze
-    @columns = Array.new(@rows[0].size) { |c| @rows.map { |row| row[c] } }.freeze
+    @rows = text.lines.map { |row| row.split.map(&:to_i) }.freeze
+    @columns = @rows.transpose.freeze
   end
 end
