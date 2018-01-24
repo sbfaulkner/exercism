07ac747a41cf6640fcd662c0bb105641d78b9ffb
S. Brent Faulkner <brent.faulkner@shopify.com>
2018-01-23 22:36:38 -0500

    lua/perfect-numbers: refactor

--- lua/perfect-numbers/perfect-numbers.lua
+++ lua/perfect-numbers/perfect-numbers.lua
@@ -1,15 +1,18 @@
 local module = {}
 
+local function factors(number)
+  return coroutine.wrap(function ()
+    for i = 1, math.floor(number / 2) do
+      if number % i == 0 then coroutine.yield(i) end
+    end
+  end)
+end
+
 function module.aliquot_sum(number)
   local sum = 0
-  local i = 1
 
-  while i < number do
-    if number % i == 0 then
-      sum = sum + i
-    end
-
-    i = i + 1
+  for f in factors(number) do
+    sum = sum + f
   end
 
   return sum
