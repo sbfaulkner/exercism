065146fe533e358e2a61d19f3c5887195deceb34
S. Brent Faulkner <brent.faulkner@shopify.com>
2020-06-13 17:16:37 -0400

    hello-world in go

---
+++ teams/shopify-edgescale/go/hello-world/hello_world.go
@@ -0,0 +1,9 @@
+// Package greeting implements the standard hello world exercise
+package greeting
+
+const greeting = "Hello, World!"
+
+// HelloWorld returns the standard greeting
+func HelloWorld() string {
+	return greeting
+}
