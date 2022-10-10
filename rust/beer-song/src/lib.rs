d28b9484cbbab0b96b283cd80570477579b46ef3
S. Brent Faulkner <sbfaulkner@gmail.com>
2022-10-09 21:51:35 -0400

    beer-song (again)

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,16 +1,28 @@
-fn bottles(n: u32) -> String {
-    match n {
-        0 => "no more bottles".to_string(),
-        1 => "1 bottle".to_string(),
-        _ => format!("{} bottles", n),
+use std::fmt;
+
+struct Bottles(u8);
+
+impl fmt::Display for Bottles {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Bottles(0) => write!(f, "no more bottles"),
+            Bottles(1) => write!(f, "1 bottle"),
+            Bottles(n) => write!(f, "{n} bottles"),
+        }
     }
 }
 
-fn take_down(n: u32) -> String {
-    match n {
-        0 => "Go to the store and buy some more".to_string(),
-        1 => "Take it down and pass it around".to_string(),
-        _ => "Take one down and pass it around".to_string(),
+struct Verse(u8);
+
+impl fmt::Display for Verse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let args = match *self {
+            Verse(0) => (Bottles(0), "Go to the store and buy some more", Bottles(99)),
+            Verse(1) => (Bottles(1), "Take it down and pass it around", Bottles(0)),
+            Verse(n) => (Bottles(n), "Take one down and pass it around", Bottles(n - 1)),
+        };
+
+        write!(f, "{0} of beer on the wall, {0} of beer.\n{1}, {2} of beer on the wall.\n", args.0, args.1, args.2)
     }
 }
 
@@ -19,14 +31,7 @@ fn capitalize(s: &str) -> String {
 }
 
 pub fn verse(n: u32) -> String {
-    capitalize(
-        format!(
-            "{0} of beer on the wall, {0} of beer.\n{1}, {2} of beer on the wall.\n",
-            bottles(n),
-            take_down(n),
-            bottles((n + 99) % 100),
-        ).as_str()
-    )
+    capitalize(&Verse(n as u8).to_string())
 }
 
 pub fn sing(start: u32, end: u32) -> String {
